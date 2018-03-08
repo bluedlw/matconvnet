0313f5dbca17e082efa5af213859c76f0c85f423
dlw <hellodailiwen@gmail.com>
2018-03-08 19:57:12 +0800

    update hard mine for any label including gt

--- matlab/+dagnn/HardMine.m
+++ matlab/+dagnn/HardMine.m
@@ -16,65 +16,57 @@ classdef HardMine < dagnn.ElementWise
         function outputs = forward(obj, inputs, params)
             obj.iter = obj.iter + 1;
             % inputs{1} class prob, inputs{2} mask/weight, e.g. bg_mask
-            bglabel = -1;
-            prob = inputs{1};
-            if size(inputs{1}, 3) == 1
-                %prob = inputs{1}; % sigmoid
-                order = 'descend';
-                %bgLabel = -1;
-            else
-                %prob = inputs{1};
-                [xmax, xid] = max(prob, [], 3);
-                prob = bsxfun(@minus, prob, xmax);
-                prob = exp(prob);
-                bglabel = 1;
-                prob = prob(:,:,1,:) ./ sum(prob, 3);
-                order = 'ascend';
-            end
-             
+            x = inputs{1};
+            inputSize = [size(x, 1) size(x, 2) size(x, 3) size(x, 4)] ;
+            labelSize = [size(x, 1), size(x, 2), 1, size(x, 4)];
             
             label = inputs{2};
-            assert(size(prob, 1) == size(label, 1) && size(prob, 2) == size(label, 2));
-            instanceMask = (label ~= 0);
-            bgMask = instanceMask & (label == bglabel);
-            fgMask = instanceMask & (label ~= bglabel);
-            %wh = size(prob, 1) * size(prob, 2);
-            hardLabel = label;
+            %hardLabel = label;
+            assert(inputSize(1) == size(label, 1) && inputSize(2) == size(label, 2));
+            
             if(obj.iter < obj.warmup)
-                outputs{1} = hardLabel;
+                outputs{1} = label;
                 return;
             end
             
-%             for n = 1 : size(prob, 4)
-%                 tmp = prob(:,:,:,n);
-%                 [~, idx] = sort(tmp(:), 'descend');
-%                 tmp2 = mask(:,:,:,n);
-%                 tmp2_ = ~tmp2;
-%                 idx = idx(tmp2(:));
-%                 if sum(tmp2_(:)) == 0
-%                     baseNum = obj.num;
-%                 else
-%                     baseNum = sum(tmp2_(:));
-%                 end
-%                 idx = idx(obj.ratio.^2 * baseNum + 1 : end);
-%                 if isempty(idx)
-%                     continue;
-%                 else
-%                     newW((n-1)*wh+idx) = 0;
-%                 end
-%             end
-
-            [~, idx] = sort(prob(:), order);
+            numPixelsPerImage = prod(inputSize(1:2));
+            numPixels = numPixelsPerImage * inputSize(4);
+            imageVolume = numPixelsPerImage * inputSize(3);
+            n = reshape(0:numPixels-1,labelSize);
+            offset = 1 + mod(n, numPixelsPerImage) + ...
+                imageVolume * fix(n / numPixelsPerImage);
+            ci = offset + numPixelsPerImage * max(c - 1,0);
+            
+            bglabel = -1;
+            order = 'descend';
+            if size(x, 3) == 1
                 
+            else
+                %order = 'ascend';
+                bglabel = 1;
+                Xmax = max(x,[],3);
+                ex = exp(bsxfun(@minus, x, Xmax));
+                loss = Xmax + log(sum(ex,3)) - x(ci);
+            end
+
+            instanceMask = (label ~= 0);
+            %bgMask = instanceMask & (label == bglabel);
+            fgMask = instanceMask & (label ~= bglabel);
+
+            [~, idx] = sort(loss(:), order);
+
             fgNum = sum(fgMask(:));
             if fgNum == 0
-                fgNum = obj.num * size(prob, 4);
+                fgNum = obj.num * size(x, 4);
             end
-            bgMask = bgMask(idx);
-            idx = idx(bgMask);
+            %bgMask = bgMask(idx);
+            %idx = idx(bgMask);
+            %idx = idx(fgNum * obj.ratio + 1 : end);
+            instanceMask = instanceMask(idx);
+            idx = idx(instanceMask);
             idx = idx(fgNum * obj.ratio + 1 : end);
-            hardLabel(idx) = 0;
-            outputs{1} = hardLabel;
+            label(idx) = 0;
+            outputs{1} = label;
         end
         
         function [derInputs, derParams] = backward(obj, inputs, params, derOutptus)
@@ -102,3 +94,22 @@ classdef HardMine < dagnn.ElementWise
     end
     
 end
+
+%             for n = 1 : size(prob, 4)
+%                 tmp = prob(:,:,:,n);
+%                 [~, idx] = sort(tmp(:), 'descend');
+%                 tmp2 = mask(:,:,:,n);
+%                 tmp2_ = ~tmp2;
+%                 idx = idx(tmp2(:));
+%                 if sum(tmp2_(:)) == 0
+%                     baseNum = obj.num;
+%                 else
+%                     baseNum = sum(tmp2_(:));
+%                 end
+%                 idx = idx(obj.ratio.^2 * baseNum + 1 : end);
+%                 if isempty(idx)
+%                     continue;
+%                 else
+%                     newW((n-1)*wh+idx) = 0;
+%                 end
+%             end
